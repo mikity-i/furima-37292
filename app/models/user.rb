8f14beecd4df0a3167cfeaef1abaf687e551f7c3
mikity-i <mi.yoshida013@gmail.com>
2022-01-10 17:31:20 +0900

    バリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,              presence: true
-  validates :user_family_name,      presence: true
-  validates :user_first_name,       presence: true
-  validates :user_family_name_kana, presence: true
-  validates :user_first_name_kana,  presence: true
+  validates :user_family_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
+  validates :user_first_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
+  validates :user_family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :user_first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,              presence: true
 
 end
