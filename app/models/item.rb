58ffd95e711fe95beb95da63f55accc58bcdcbb9
mikity-i <mi.yoshida013@gmail.com>
2022-01-12 19:40:14 +0900

    カテゴリーのアクティブハッシュを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  
+  validates :category_id, numericality: { other_than: 1 }
+
 end
