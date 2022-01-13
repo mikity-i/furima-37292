6b271df98e06c97bd4b83cd301d5e46624ef9c8d
mikity-i <mi.yoshida013@gmail.com>
2022-01-13 12:10:50 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,7 @@ class Item < ApplicationRecord
   belongs_to :category, :item_status, :shipping_fee, :prefecture, :shipping_date
   
   validates :category_id, :item_status_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 }
+  validates :image, :item_name, :introduction, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 end
