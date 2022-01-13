47b107f5e1a4474f2be42cbe7d052de06f92bc88
mikity-i <mi.yoshida013@gmail.com>
2022-01-13 13:20:33 +0900

    ItemモデルのFactoryBotの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name {Faker::Book.title}
+    introduction {Faker::String.random}
+
+    category_id {2}
+    item_status_id {2}
+    shipping_fee_id {2}
+    prefecture_id {2}
+    shipping_date_id {2}
+
+    price {Faker::Number.between(from: 300, to: 9999999)}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
   end
 end
