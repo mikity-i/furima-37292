182b91ad879ba953412e1237af12df34fa59d7ba
mikity-i <mi.yoshida013@gmail.com>
2022-01-13 19:31:04 +0900

    バリデーションの追記
    priceにて小数を入力できないようにした

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,6 @@ class Item < ApplicationRecord
   
   validates :category_id, :item_status_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 }
   validates :image, :item_name, :introduction, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 end
