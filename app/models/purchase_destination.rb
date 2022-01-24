f1b2015c41889804382e829ddd8c528014ba23aa
mikity-i <mi.yoshida013@gmail.com>
2022-01-24 13:15:32 +0900

    バリデーションの追記

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -5,10 +5,10 @@ class PurchaseDestination
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :post_code
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :city
     validates :block
-    validates :phone_number
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
